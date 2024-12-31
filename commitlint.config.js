9ee412b97425e88588825c6394ef113b367e46a2
Natnael Mesele <nmesele5@gmail.com>
2024-12-31 17:36:09 +0300

    🔨 script(lint-staged): add the lint-staged command to the pre-commit hook on husky

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,31 @@
-export default { extends: ['@commitlint/config-conventional'] };
+export default {
+    parserPreset: {
+        parserOpts: {
+            headerPattern: /^(\p{Emoji_Presentation}|\p{Emoji}\uFE0F?)\s*(\w+)(?:$(.*)$)?!?:\s(.*)$/u,
+            headerCorrespondence: ['emoji', 'type', 'scope', 'subject'],
+        },
+    },
+    rules: {
+        'type-enum': [
+            2,
+            'always',
+            [
+                'fix', 'feat', 'docs', 'style', 'refactor', 'perf', 'test',
+                'build', 'ci', 'revert', 'dump', 'hotfix', 'deploy', 'ui',
+                'init', 'security', 'secret', 'bump', 'fix-lint', 'wip',
+                'dep-drop', 'dep-bump', 'pin', 'analytics', 'dep-add',
+                'dep-rm', 'config', 'script', 'lang', 'typo', 'poop',
+                'merge', 'package', 'external', 'resource', 'license',
+                'boom', 'asset', 'accessibility', 'source-docs', 
+                'beer', 'text', 'db', 'logs-add', 'logs-rm',
+                'people', 'ux', 'arch', 'design', 'mock',
+                'egg', 'ignore', 'snap', 'experiment',
+                'seo', 'types', 'seed',  'flag',
+                'catch','animation','deprecation','auth',
+                'fix-simple','exploration','dead','test-fail',
+                'logic','health','infra','devxp',
+                'money','threading','validation','chore'
+            ],
+        ],
+    },
+};
